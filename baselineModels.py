ed1651944d9cb4e52e5ce3b537bcfbc964912f09
Anders Greve Soerensen <anders1709@gmail.com>
2025-03-26 15:51:47 +0100

    Modify logistic regression to work correctly. Add Kfold CV for testing

--- baselineModels.py
+++ baselineModels.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class LogisticRegression:
+class BaseLogisticRegression:
     def __init__(self, learning_rate=0.01, num_iterations=1000):
         self.learning_rate = learning_rate
         self.num_iterations=num_iterations
@@ -8,28 +8,21 @@ class LogisticRegression:
 
     def sigmoid(self, x):
         return 1 / (1+np.exp(-x))
-    
-    def predict_proba(self, X):
-        X = np.hstack([np.ones([X.shape[0], 1]), X])
-        
-        return self.sigmoid(X @ self.weights)
-
 
     def fit(self, X, y):
         X = np.hstack([np.ones([X.shape[0], 1]), X])
 
         num_samples, num_features = X.shape
         self.weights = np.ones(num_features)
-        
-        p = self.predict_proba(X)
-        lossGradient = X.T * (y - p)
         for _ in range(self.num_iterations):
+            p = self.sigmoid(X @ self.weights)
+            lossGradient = X.T @ (y - p) / num_samples
             self.weights = self.weights + self.learning_rate * (lossGradient)
 
-    def predict(self, X):
+    def predict(self, X, threshold=0.5):
         X = np.hstack([np.ones([X.shape[0], 1]), X])
 
         linear_combination = X @ self.weights
         activated = self.sigmoid(linear_combination)
-        prediction = activated > 0.5
+        prediction = activated > threshold
         return prediction
